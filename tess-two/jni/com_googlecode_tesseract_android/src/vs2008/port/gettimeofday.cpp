7dc3c92a64baa7f6509b8ed2d52ae85ec5b393fb
Werner Trobin <werner.trobin@dacuda.com>
2013-04-24 10:29:49 +0200

    gettimeofday() for Windows RT

--- tess-two/jni/com_googlecode_tesseract_android/src/vs2008/port/gettimeofday.cpp
+++ tess-two/jni/com_googlecode_tesseract_android/src/vs2008/port/gettimeofday.cpp
@@ -21,6 +21,41 @@
 #include "gettimeofday.h"
 
 #ifdef WINAPI_FAMILY
+#include <windows.h>
+#include <cstdint>
+
+
+int gettimeofday(struct timeval *tp, struct timezone *tzp)
+{
+	if(tp == NULL)
+	{
+		return -1;
+	}
+
+	FILETIME systemTime;
+	GetSystemTimeAsFileTime(&systemTime);
+
+	SYSTEMTIME epochStart;
+	epochStart.wYear = 1970;
+	epochStart.wMonth = 1;
+	epochStart.wDayOfWeek = 4;
+	epochStart.wDay = 1;
+	epochStart.wHour = 0;
+	epochStart.wMinute = 0;
+	epochStart.wSecond = 0;
+	epochStart.wMilliseconds = 0;
+
+	FILETIME epochStartFt;
+	SystemTimeToFileTime(&epochStart, &epochStartFt);
+
+	const uint64_t start = static_cast<uint64_t>(epochStartFt.dwHighDateTime) << 32 + static_cast<uint64_t>(epochStartFt.dwLowDateTime);
+	const uint64_t now = static_cast<uint64_t>(systemTime.dwHighDateTime) << 32 + static_cast<uint64_t>(systemTime.dwLowDateTime);
+	const uint64_t diff = now - start; // Increments since the begin of the Unix epoch in 100ns steps
+
+	tp->tv_sec = diff / 10*1000*1000;
+	tp->tv_usec = (diff - 10*1000*1000*static_cast<uint64_t>(tp->tv_sec)) / 10;
+	return 0;
+}
 #else
 int gettimeofday(struct timeval *tp, struct timezone *tzp) {
   l_int32 sec, usec;
